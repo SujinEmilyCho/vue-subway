99541b17bdaa77f15cbed637454edb1f3f4d7a38
sujin <sujinemilycho@gmail.com>
2020-07-11 17:15:06 +0900

    chore: vue-rounter 의존성 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueRouter from 'vue-router';
+
+const router = new VueRouter({});
 
 new Vue({
-	el: '#app',
-	render: h => h(App),
-});
+  router,
+  render: h => h(App),
+}).$mount('#app');
